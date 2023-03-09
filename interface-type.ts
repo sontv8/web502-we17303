646348b4b5d4cf5fc74ce07882f3f2ee8fc13782
sontv8 <sontv8@fpt.edu.vn>
2023-03-09 16:02:56 +0700

    Buổi 2: interface + type final

--- interface-type.ts
+++ interface-type.ts
@@ -1,81 +1,97 @@
-export { }
+// export { }
 
 
-// định nghĩa kiểu dữ liệu với interface
-/* Object */
+// // định nghĩa kiểu dữ liệu với interface
+// /* Object */
 
+
+// // interface IProject {
+// //     id: number,
+// //     name: string,
+// //     image: string,
+// //     link: string,
+// //     status: boolean
+// // }
+// // const project: IProject = {
+// //     id: 1,
+// //     name: "Du an 1",
+// //     image: "https://picsum.photos/200/200",
+// //     link: "https://picsum.photos/200/200",
+// //     status: true
+// // }
+
+// // function getData(props: IProject): IProject {
+// //     return props;
+// // }
+// // const addData = (data: IProject) => {
+
+// // }
+// // getData(project)
+
+// /* Array */
 
 // interface IProject {
 //     id: number,
 //     name: string,
-//     image: string,
-//     link: string,
-//     status: boolean
+//     email?: string
 // }
-// const project: IProject = {
+// // const projectList: IProject[] = [
+// //     { id: 1, name: "Du an 1", email: "" },
+// //     { id: 2, name: "Du an 2" }
+// // ]
+
+// // const projectList: Array<IProject> = [
+// //     { id: 1, name: "Du an 1" },
+// //     { id: 2, name: "123" }
+// // ]
+
+// /* Function*/
+// interface ISumFunction {
+//     (a: number, b: number): string
+// }
+
+// const sum: ISumFunction = (a, b) => {
+//     return "12"
+// }
+// sum(10, 20)
+
+// interface IGetProductFunc {
+//     (data: IProject[]): {}
+// }
+// const getProduct: IGetProductFunc = (data) => {
+//     return data
+// }
+// getProduct([{ id: 1, name: "abc" }, { id: 2, name: "abc" }])
+
+// // định nghĩa kiểu dữ liệu với type
+// /* Object */
+// type TProduct = {
+//     id: number,
+//     name: string
+// }
+// const product: TProduct = {
 //     id: 1,
-//     name: "Du an 1",
-//     image: "https://picsum.photos/200/200",
-//     link: "https://picsum.photos/200/200",
-//     status: true
+//     name: "abc"
 // }
-
-// function getData(props: IProject): IProject {
-//     return props;
-// }
-// const addData = (data: IProject) => {
-
-// }
-// getData(project)
-
-/* Array */
-
-interface IProject {
-    id: number,
-    name: string,
-    email?: string
-}
-// const projectList: IProject[] = [
-//     { id: 1, name: "Du an 1", email: "" },
-//     { id: 2, name: "Du an 2" }
+// /* Array */
+// type TProductList = TProduct[]
+// const projectList: TProductList = [
+//     { id: 1, name: "abc" }
 // ]
+// /* Function */ 
 
-// const projectList: Array<IProject> = [
-//     { id: 1, name: "Du an 1" },
-//     { id: 2, name: "123" }
-// ]
 
-/* Function*/
-interface ISumFunction {
-    (a: number, b: number): string
+interface IProduct {
+    id?: number;
+    name: string;
 }
-
-const sum: ISumFunction = (a, b) => {
-    return "12"
-}
-sum(10, 20)
-
-interface IGetProductFunc {
-    (data: IProject[]): {}
-}
-const getProduct: IGetProductFunc = (data) => {
-    return data
-}
-getProduct([{ id: 1, name: "abc" }, { id: 2, name: "abc" }])
-
-// định nghĩa kiểu dữ liệu với type
-/* Object */
-type TProduct = {
-    id: number,
-    name: string
-}
-const product: TProduct = {
-    id: 1,
-    name: "abc"
-}
-/* Array */
-type TProductList = TProduct[]
-const projectList: TProductList = [
-    { id: 1, name: "abc" }
+const products = [
+    { id: 1, name: "Proudct A" }, { id: 1, name: "Proudct B" }
 ]
-/* Function */ 
+function showProduct(result: IProduct[]): void {
+    const app = document.querySelector('#app') as HTMLElement | null;
+    if (app) {
+        app.innerHTML = result.map((item: IProduct) => `${item.name}`)
+    }
+}
+showProduct(products);
