1bef1a2c710aa00724a6b0135de5349b78c0e380
sontv8 <sontv8@fpt.edu.vn>
2023-03-16 16:01:17 +0700

    Buổi 5: delete product

--- WE17303-react-ts/src/pages/Product.tsx
+++ WE17303-react-ts/src/pages/Product.tsx
@@ -1,13 +1,17 @@
 
 
 const ProductPage = (props) => {
-
+    const removeProduct = (id) => {
+        fetch('http://localhost:3000/products/' + id, {
+            method: 'DELETE'
+        })
+    }
     return (
         <div>
             {props.products.map((item) => {
                 return <div key={item.id}>
                     <h2>{item.name}</h2>
-                    <button>Remove</button>
+                    <button onClick={() => removeProduct(item.id)}>Remove</button>
                 </div>
 
             })}
