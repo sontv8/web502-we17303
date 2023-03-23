24ece1aa15b467ca87324668f6fc460b8e13f810
sontv8 <sontv8@fpt.edu.vn>
2023-03-23 15:55:31 +0700

    Buổi 6: thêm mới sản phẩm - xử lý nhiều input

--- WE17303-react-ts/src/pages/admin/AddProduct.tsx
+++ WE17303-react-ts/src/pages/admin/AddProduct.tsx
@@ -6,18 +6,27 @@ const AddProductPage = (props) => { // nhận props từ App.tsx
     const navigate = useNavigate() // khởi tạo navigate để điều hướng
     const [inputValue, setInputValue] = useState({}) // khởi tạo state để lưu giá trị input
     const onHandleChange = (e) => { // hàm xử lý sự kiện khi input thay đổi
-        setInputValue({ name: e.target.value }); // set giá trị cho state
+        const name = e.target.name;
+        const value = e.target.value;
+        setInputValue({ ...inputValue, [name]: value });
+        // n -> n
+        // e -> e
+
+        // setInputValue({ name: e.target.value }); // set giá trị cho state
 
     }
     const onHandleSubmit = (e) => { // hàm xử lý sự kiện khi submit form
         e.preventDefault()
+        // console.log(inputValue);
+
         props.onAdd(inputValue); // gọi hàm onAdd từ props truyền vào
         navigate('/admin/products') // điều hướng đến trang /admin/products
     }
     return (
         <div>
             <form action="" onSubmit={onHandleSubmit}>
-                <input type="text" placeholder='Product Name' onChange={onHandleChange} />
+                <input type="text" placeholder='Product Name' onChange={onHandleChange} name='name' />
+                <input type="number" onChange={onHandleChange} name='price' />
                 <button type="submit">Add New Product</button>
             </form>
         </div>
