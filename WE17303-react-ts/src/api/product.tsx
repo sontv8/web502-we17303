1a3857f56c23eeeda2c911c33a2f387debb78ab7
sontv8 <sontv8@fpt.edu.vn>
2023-04-01 14:47:55 +0700

    Buổi 12: ant design + định nghĩa type cho tham số

--- WE17303-react-ts/src/api/product.tsx
+++ WE17303-react-ts/src/api/product.tsx
@@ -1,18 +1,22 @@
 import instance from "./instance";
-
+interface IProduct {
+    id: number,
+    name: string,
+    price: number
+}
 const getAllProduct = () => {
     return instance.get('/products');
 }
-const getOneProduct = () => {
+const getOneProduct = (id: number) => {
     return instance.get('/products/' + id);
 }
-const addProduct = (product) => {
+const addProduct = (product: IProduct) => {
     return instance.post('/products', product);
 }
-const updateProduct = (product) => {
+const updateProduct = (product: IProduct) => {
     return instance.put('/products/' + product.id, product);
 }
-const deleteProduct = (id) => {
+const deleteProduct = (id: number) => {
     return instance.delete('/products/' + id);
 }
 
--- WE17303-react-ts/src/pages/admin/AddProduct.tsx
+++ WE17303-react-ts/src/pages/admin/AddProduct.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
+import { Button, Checkbox, Form, Input } from 'antd';
 
 // const AddProductPage = (props) => { // nhận props từ App.tsx 
 //     // props - onAdd
@@ -34,22 +35,73 @@ import { useForm } from 'react-hook-form'
 //     )
 // }
 
+interface IProduct {
+    id: number,
+    name: string,
+    price: number
+}
+interface IProps {
+    onAdd: (product: IProduct) => void
+}
+const AddProductPage = (props: IProps) => { // nhận props từ App.tsx 
+    /*
+        {
 
-
-const AddProductPage = (props) => { // nhận props từ App.tsx 
+        }
+    */
     const navigate = useNavigate() // khởi tạo navigate để điều hướng
-    const { register, handleSubmit } = useForm()
-    const onHandleSubmit = (data) => {
-        props.onAdd(data);
-        navigate('/admin/products')
-    }
+    // const { register, handleSubmit } = useForm()
+    // const onHandleSubmit = (data) => {
+    //     props.onAdd(data);
+    //     navigate('/admin/products')
+    // }
+
+    const onFinish = (values: any) => {
+        props.onAdd(values);
+    };
+
+    const onFinishFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
     return (
         <div>
-            <form action="" onSubmit={handleSubmit(onHandleSubmit)}>
+            {/* <form action="" onSubmit={handleSubmit(onHandleSubmit)}>
                 <input type="text" placeholder='Product Name' {...register('name')} />
                 <input type="number" {...register('price')} />
                 <button type="submit">Add New Product</button>
-            </form>
+            </form> */}
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                style={{ width: 1000, margin: '0 auto' }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Product Name"
+                    name="name"
+                    rules={[{ required: true, message: 'Please input your username!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Product Price"
+                    name="price"
+                    rules={[{ required: true, message: 'Please input your password!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Add New Product
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     )
 }
